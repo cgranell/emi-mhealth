222f6f32d84a96c5c0747d45978f6f5a1d9f4599
cgranell <canut@uji.es>
2018-11-08 13:55:42 +0100

    Udpate documentation

---
+++ emidata/R/emidata-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+#'
+#'
+#' Papers.
+#'
+#' All selected papers  This includes all names with at
+#' least 5 uses.
+#'
+#' @format A data frame with nine variables: \code{type}, \code{title},
+#'   \code{asbtract}, \code{journal}, \code{author}, \code{year} and \code{keywords}).
+"papers"
+
