735c1884ff9b90e233c2896f88918404bbd9baf2
cgranell <canut@uji.es>
2018-11-08 14:28:35 +0100

    Created data documentation

---
+++ emidata/R/data.R
@@ -0,0 +1,24 @@
+
+#' Papers
+#'
+#' List of selected papers included in the review (N=136).
+#'
+#' @source scientific databases such as Scopus, Web of Science, and PubMed.
+#' @format A data frame with nine variables:
+#' \describe{
+#'   \item{\code{filename}{Full filename of the paper.}
+#'   \item{\code{id}}{A unique identifier.}
+#'   \item{\code{type}}{Article or InProceedings , same syntax as bibtex.}
+#'   \item{\code{title}}{Title of the paper.}
+#'   \item{\code{abstract}}{Asbtract of the paper.}
+#'   \item{\code{journal}}{Name of the jpurnal if journal article, otherwise NA if conference paper.}
+#'   \item{\code{author}}{List of authors separated y "and"}
+#'   \item{\code{year}}{Publishing year of the paper, a value between 2013 and 2018}
+#'   \item{\code{keywords}}{List of keyworkd separated by coma, otherwise NA}
+#'
+#' }
+#' @examples
+#' \dontrun{
+#'   papers
+#'   }
+"papers"
