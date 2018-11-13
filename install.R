9d6d09e54dded9378f96f50aa72277dd8d12b9eb
Carlos Granell <canut@uji.es>
2018-11-13 11:00:36 +0100

    Added EDA functions
    glimpse( ) and skimr::skim()

--- install.R
+++ install.R
@@ -3,6 +3,9 @@ install.packages("tidytext")
 install.packages("kableExtra")
 install.packages("stringr")
 install.packages("wordcloud")
-
+install.packages("skimr")
+#install.packages(grid)
+#install.packages(gridBase
+#install.packages(gridExtra)
 
 
